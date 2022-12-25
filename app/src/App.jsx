4b3d5aaabebe8c34be108856fac8f32d0ec249a7
Chingun <97659944+solac911@users.noreply.github.com>
2022-12-26 02:07:48 +0800

    installation

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl flex justify-center">
+      Hello this is installation!
+    </div>
+  );
+}
+
+export default App;
